0552386e02ac083cc62ecf5dbf3a6a9d0ccaf056
Kevin Tran <kevin.tran@leonardo.com>
2018-05-30 17:10:36 -0400

    Copy source code from my personal Gitlab. It can support @Timed in @Service with TimedAspect and a good example for tagged counter (success counter & fail counter)

---
+++ prometheus-aspectj/src/test/java/org/tnmk/practice/springboot/prometheuse/aspectj/StartWebApplicationTests.java
@@ -0,0 +1,16 @@
+package org.tnmk.practice.springboot.prometheuse.aspectj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StartWebApplicationTests {
+
+    @Test
+    public void startContext() {
+    }
+
+}
